61eedf7341defe0282a658bcdb2660b454f2bfa8
zhangjanxiong <1184159464@qq.com>
2019-11-26 13:12:56 +0800

    跨域解决

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+//跨域解决
+app.all('*', function(req, res, next) {
+    console.log(req.method);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Content-type');
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
+    res.header('Access-Control-Max-Age',1728000);//预请求缓存20天
+    next();  
+});
+
 //回调passport.js文件中
 require('./config/passport')(passport);
 
